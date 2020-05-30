ce6be3c38ba1ea8102769fe5558ea60fac3c67a0
parkj15 <jamespark710@gmail.com>
2020-05-30 15:03:27 -0400

    Update About page

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -3,19 +3,29 @@ import React from 'react';
 import classes from './About.module.css';
 
 const about = () => (
-  <div className={classes.About}>
+  <div className={classes.Section}>
     <h3>About this App</h3>
-    <p>Just a simple cost basis calculator for stocks.</p>
+    <p>
+      Simple React app (with no database) designed to help with cost
+      basis calculations for stock positions.
+    </p>
     <br/>
-
-    <h3>Planned Features</h3>
-    <p>Reach out to stock API to provide useful information.</p>
+    <h3>Upcoming Changes</h3>
+    <p>Integrate with stock API to provide useful information.</p>
+    <ul>
+      <li>Total equity</li>
+      <li>Total return percentage</li>
+      <li>Last day stock hit desired share price.</li>
+    </ul>
     <p>Open to suggestions!</p>
     <br/>
-
-    <h3>Project Goals (Growing List)</h3>
-    <p>Build and deploy native React app.</p>
-    <p>Use third-party API.</p>
+    <h3>But, why?</h3>
+    <p>Build native React app.</p>
+    <ul>
+      <li>Practice React, JSX, and CSS.</li>
+    </ul>
+    <p>Deploy app to Heroku.</p>
+    <p>(WIP) Integrate third-party API.</p>
   </div>
 );
 
